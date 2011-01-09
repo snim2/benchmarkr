e118a4b3e399b08f7872de90653538cbb4e06289
snim2 <mount.sarah@gmail.com>
2011-01-09 02:58:51 +0000

    Allow use of module as script.

---
+++ benchmarkr/__main__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+Copyright (C) Sarah Mount, 2010.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+__author__ = 'Sarah Mount <s.mount@wlv.ac.uk>'
+__date__ = 'Jan 2011'
+
+
+if __name__ == '__main__':
+    import benchmarkr
+    benchmarkr.main()
